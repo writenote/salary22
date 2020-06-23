27046cce97bfc8844e0a5d4bf4d753d1eda67ccc
Baeblue <seungmin941215@gmail.com>
2020-06-23 11:29:48 +0900

    Add comment

--- src/main/java/com/example/model/Salary.java
+++ src/main/java/com/example/model/Salary.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 
@@ -57,11 +56,11 @@ public class Salary {
     public Integer calDailyWage(String startTime, String endTime) {
         dailyWage = 0;
 
-        startTime = startTime.substring(11, 13);
-        endTime = endTime.substring(11, 13);
+        startTime = startTime.substring(11, 13);   // 21
+        endTime = endTime.substring(11, 13);   // 09
 
-        Integer startWorkTime = Integer.parseInt(startTime);
-        Integer endWorkTime = Integer.parseInt(endTime);
+        Integer startWorkTime = Integer.parseInt(startTime);   // 21
+        Integer endWorkTime = Integer.parseInt(endTime);   // 9
 
         System.out.println("startWorkTime: " + startWorkTime);
         System.out.println("endWorkTime: " + endWorkTime);
@@ -69,11 +68,11 @@ public class Salary {
         List<Boolean> timeList = new ArrayList<>(Arrays.asList(new Boolean[24]));  // index 0~23
         Collections.fill(timeList, Boolean.FALSE);
 
-        if(startWorkTime < endWorkTime) {
+        if(startWorkTime < endWorkTime) {   // 근무 시간이 하루 안에 시작 및 종료
             for(int i=startWorkTime; i<endWorkTime; i++) {
                 timeList.set(i, Boolean.TRUE);
             }
-        } else {
+        } else {   // 근무 시간이 자정을 넘어감
             for(int i=startWorkTime; i<=23; i++) {
                 timeList.set(i, Boolean.TRUE);
             }
@@ -86,15 +85,15 @@ public class Salary {
         for(int i=0; i<=23; i++) {
             if(timeList.get(i) == Boolean.TRUE) {
                 if(i>=6 && i<=21) {
-                    timeMap.put(i, 8950);
+                    timeMap.put(i, 8950);   // 주간 기본 시급
                 } else {
-                    timeMap.put(i, 12885);
+                    timeMap.put(i, 12885);   // 야간 수당
                 }
             }
         }
 
-        dailyWage = timeMap.values().stream().mapToInt(i -> i).sum();   // 1번
-        //dailyWage = timeMap.values().stream().reduce(0, Integer::sum);   //2번
+        //dailyWage = timeMap.values().stream().mapToInt(i -> i).sum();   // 1번
+        dailyWage = timeMap.values().stream().reduce(0, Integer::sum);   //2번
 
         return dailyWage;
     }
