27a54386ea03dc22bdf618a377af655d3c679172
Baeblue <seungmin941215@gmail.com>
2020-06-29 00:51:56 +0900

    Update MonthlyPay

--- src/front/src/components/pay/MonthlyPay.vue
+++ src/front/src/components/pay/MonthlyPay.vue
@@ -3,13 +3,171 @@
     <div class="subject">
       <h3>월급 계산하기</h3>
     </div>
+    <div class="inputForm">
+      <div class="hourlyWageForm">
+        <label for="hourlyWage">시급</label>
+        <input type="text" class="form-control" placeholder="최저시급 8,590원" id="hourlyWage"
+               v-model="hourlyWage"/>
+      </div>
+
+      <div class="startTime">
+        <label for="startTime">시작 시간</label>
+        <datetime type="time" id="startTime" placeholder="시간 선택"
+                  value-zone="Asia/Seoul"
+                  :minute-step="60"
+                  v-model="startTime">  <!--class="theme-orange"-->
+        </datetime>
+      </div>
+
+      <div class="endTime">
+        <label for="endTime">종료 시간</label>
+        <datetime type="time" id="endTime" placeholder="시간 선택"
+                  value-zone="Asia/Seoul"
+                  :minute-step="60"
+                  v-model="endTime">
+        </datetime>
+      </div>
+
+      <div class="workingDays">   <!-- 너무 길어서 다른 모듈 찾아보기 -->
+        <label>월 근무 일수</label>
+        <b-dropdown
+          id="workingDays" class="m-2" required
+          :text="selectedDays"
+          v-model="days"
+        >
+          <b-dropdown-item
+            v-for="days in daysList" :key="days.id"
+            @click="selectDays(days)"
+          >
+            {{ days }}
+          </b-dropdown-item>
+        </b-dropdown>
+      </div>
+    </div>
+
+    <div class="btnArea">
+      <button @click="calMonthlyPay(hourlyWage, startTime, endTime, days)" class="btn btn-success">계산하기</button>
+    </div>
+
+
+    <div class="resultArea" v-show="result">
+      <h5>월 총 근무시간은 하루 {{ dailyTotalTime }}시간 X {{ days }}일로 총 {{ monthlyTotalTime }}시간입니다.</h5>
+      <h5> - 기본 수당: 일급 {{ dailyPay }}원 X {{ days }}일 = {{ basicDailyPay }}원</h5>
+      <h5> - 예상 주휴 수당: 한 주 주휴수당 {{ holidayPay }}원 X 4주 = {{ holidayTotalPay }}원</h5>
+      <h5>총 예상 월급은 {{ monthlyPay }}원입니다.</h5>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: "MonthlyPay",
+    data() {
+      return {
+        result: false,
+        hourlyWage: null,
+        startTime: "",
+        endTime: "",
+        days: "",
+        dailyPay: 0,
+        monthlyPay: 0,
+        dailyTotalTime: 0,
+        weeklyTotalTime: 0,
+        monthlyTotalTime: 0,
+        basicDailyPay: 0,
+        holidayPay: 0,
+        holidayTotalPay: 0,
+        selectedDays: '근무 일수 선택',
+        daysList: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15',
+              '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+      }
+    },
+    methods: {
+      selectDays(selectedItem) {
+        this.selectedDays = selectedItem;
+        this.days = this.selectedDays;
+      },
+      calMonthlyPay(hourlyWage, startTime, endTime, days) {
 
+        if(hourlyWage == null) {
+          hourlyWage = 8590;
+        } else {
+          hourlyWage = parseInt(hourlyWage);
+        }
+
+        const formatStartTime = startTime.substring(11, 13);   // 2020-06-26T09:00:00.000+09:00 -> 09
+        const formatEndTime = endTime.substring(11, 13);   // 12
+
+        const workStartTime = parseInt(formatStartTime);   // 9
+        const workEndTime = parseInt(formatEndTime);   // 12
+
+        let timeList = new Array(24);
+        for(let i=0; i<=23; i++) {
+          timeList[i] = Boolean(false);
+        }
+
+        if(workStartTime < workEndTime) {   // 근무 시간이 하루 안에 시작 및 종료
+          for(let i=workStartTime; i<workEndTime; i++) {
+            timeList[i] = Boolean(true);
+          }
+        } else {   // 근무 시간이 자정을 넘어감
+          for(let i=workStartTime; i<=23; i++) {
+            timeList[i] = Boolean(true);
+          }
+          for(let i=0; i<workEndTime; i++) {
+            timeList[i] = Boolean(true);
+          }
+        }
+
+        let timeMap = new Map();
+        for(let i=0; i<=23; i++) {
+          if(timeList[i] == Boolean(true)) {
+            this.dailyTotalTime++;
+            if(i>=6 && i<=21) {
+              timeMap.set(i, hourlyWage);   // 주간 기본 시급
+            } else {
+              timeMap.set(i, (hourlyWage*1.5));   // 야간 수당
+            }
+          }
+        }
+
+        let sum=0;
+        timeMap.forEach(function (value) {
+          sum += value;
+        });
+        this.dailyPay = sum;
+        this.weeklyTotalTime = this.dailyTotalTime * parseInt(days/4);
+        console.log("주 총 근무시간: " + this.weeklyTotalTime);
+        this.monthlyTotalTime = this.dailyTotalTime * days;
+        this.basicDailyPay = this.dailyPay * days;
+
+        if(this.weeklyTotalTime >= 15 && this.weeklyTotalTime < 40) {
+          this.holidayPay = (this.weeklyTotalTime / 40) * 8 * hourlyWage;
+        } else if(this.weeklyTotalTime >= 40) {
+          this.holidayPay = 8 * hourlyWage;
+        }
+        console.log("한 주 주휴수당: " + this.holidayPay);
+        this.monthlyPay = this.basicDailyPay + (this.holidayPay*4);
+        this.holidayTotalPay = this.holidayPay * 4;
+        this.dailyPay = sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        this.basicDailyPay = this.basicDailyPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        this.holidayPay = this.holidayPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        this.holidayTotalPay = this.holidayTotalPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        this.monthlyPay = this.monthlyPay.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        // this.formatPay(this.dailyPay);
+        // this.formatPay(this.basicDailyPay);
+        // this.formatPay(this.holidayPay);
+        // this.formatPay(this.weeklyPay);
+
+        this.result = true;
+      },
+      formatPay(pay) {
+        pay = pay.toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        return pay;
+      }
+    }
   }
 </script>
 
@@ -22,6 +180,85 @@
       width: 1000px;
       margin: auto;
       display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .inputForm {
+
+      justify-content: center;   // 화면 중앙
+      text-align: center;
+      max-width: 500px;
+      margin: auto;
+
+      .hourlyWageForm {
+
+        padding-top: 30px;
+        display: flex;   // 옆으로 나란히
+        align-items: center;   // 위아래 중앙
+
+        label {
+          min-width: 100px;
+          margin: auto;
+          text-align: left;
+        }
+
+      }
+
+      .startTime {
+        padding-top: 30px;
+        display: flex;
+        align-items: center;
+
+        label {
+          min-width: 100px;
+          margin-left: 0px;
+          text-align: left;
+        }
+
+        dateTime {
+          //width: 500px;
+        }
+      }
+
+      .endTime {
+        padding-top: 30px;
+        display: flex;
+        align-items: center;
+
+        label {
+          min-width: 100px;
+          margin-left: 0px;
+          text-align: left;
+        }
+      }
+
+      .workingDays {
+        padding-top: 30px;
+        display: flex;
+        align-items: center;
+
+        label {
+          min-width: 100px;
+          margin-left: 0px;
+          text-align: left;
+        }
+      }
+    }
+
+    .btnArea {
+      padding-top: 30px;
+      display: flex;
+      width: 100px;
+      justify-content: center;
+      font-size: 18px;
+      margin: 0 auto;
+    }
+
+    .resultArea {
+      padding-top: 50px;
+      width: 1000px;
+      margin: auto;
       align-items: center;
       justify-content: space-between;
     }
